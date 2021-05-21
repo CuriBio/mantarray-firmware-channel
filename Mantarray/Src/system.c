e24032c8d23c8e8531d78e462ee94a5a708763c5
Farshad2021 <farshad@curiio.com>
2021-05-21 15:30:47 -0700

    i2c problem solved

--- Mantarray/Src/system.c
+++ Mantarray/Src/system.c
@@ -10,7 +10,9 @@ extern System my_sys;
 
 void module_system_init(System *thisSystem)
 {
-	my_sys.data_bus = internal_bus_create(GPIOB,0x0f,BUS_CLK_GPIO_Port,BUS_CLK_Pin,BUS_CLK_GPIO_Port,BUS_C0_Pin);
+	my_sys.data_bus = internal_bus_create(GPIOB, 0xFF,
+											  BUS_CLK_GPIO_Port, BUS_CLK_Pin,
+											  BUS_C1_GPIO_Port, BUS_C1_Pin);
 
 
 
@@ -37,13 +39,12 @@ void module_system_init(System *thisSystem)
 
 void state_machine(System *thisSystem)
 {
-	uint8_t testData[23] = {3,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,1};
-	//TODO Link data output to magnetometer memory instead
-	uint32_t temp = 0;
+	uint8_t testData[23] = {3,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2,1};  //TODO remove after Link data output to magnetometer memory instead
+
 
 	while(1)
 	{
-	internal_bus_write_data_frame(my_sys.data_bus,testData,22);HAL_Delay(500);
+	//internal_bus_write_data_frame(my_sys.data_bus,testData,22);HAL_Delay(500);
 		if(my_sys.i2c_line->buffer_index)
 		{
 			switch(my_sys.i2c_line->receiveBuffer[0])
@@ -53,7 +54,7 @@ void state_machine(System *thisSystem)
 				{
 
 					//TODO Link data output to magnetometer memory instead
-					internal_bus_write_data_frame(my_sys.data_bus,testData,10);
+					internal_bus_write_data_frame(my_sys.data_bus,testData,22);
 					break;
 				}
 				//-------------------------------
@@ -168,50 +169,6 @@ void state_machine(System *thisSystem)
 			case MODULE_SYSTEM_STATUS_IDLE:
 				if (thisSystem->BUS_FLAG == 1)
 				{
-					//GPIOC->BSRR = GPIO_PIN_0;
-					//GPIOC->BRR = GPIO_PIN_0;
-					//Set Bus pins to output
-					//temp = GPIOB->MODER;
-					//temp &= ~BUS_BUSMASK32;
-					//temp |= BUS_ACK_MODER;
-					//GPIOB->MODER = temp;
-					//Set CBus pins to output
-					//temp = GPIOA->MODER;
-					//temp &= ~BUS_CBUSMASK32;
-					//temp |= BUS_CACK_MODER;
-					//GPIOA->MODER = temp;
-					//Send acknowledge
-					GPIOA->BSRR = GPIO_PIN_15;
-					//GPIOA->BSRR = 0x00002000;
-					//Acknowledge(&thisSystem->Bus);
-					//GPIOC->BSRR = GPIO_PIN_0;
-					//GPIOC->BRR = GPIO_PIN_0;
-					//Send dataframe
-					GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[0]) | ((0x000000FF & ~testData[0]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[1]) | ((0x000000FF & ~testData[1]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[2]) | ((0x000000FF & ~testData[2]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[3]) | ((0x000000FF & ~testData[3]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[4]) | ((0x000000FF & ~testData[4]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[5]) | ((0x000000FF & ~testData[5]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[6]) | ((0x000000FF & ~testData[6]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[7]) | ((0x000000FF & ~testData[7]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[8]) | ((0x000000FF & ~testData[8]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[9]) | ((0x000000FF & ~testData[9]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[10]) | ((0x000000FF & ~testData[10]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[11]) | ((0x000000FF & ~testData[11]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[12]) | ((0x000000FF & ~testData[12]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[13]) | ((0x000000FF & ~testData[13]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[14]) | ((0x000000FF & ~testData[14]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[15]) | ((0x000000FF & ~testData[15]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[16]) | ((0x000000FF & ~testData[16]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[17]) | ((0x000000FF & ~testData[17]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[18]) | ((0x000000FF & ~testData[18]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[19]) | ((0x000000FF & ~testData[19]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[20]) | ((0x000000FF & ~testData[20]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[21]) | ((0x000000FF & ~testData[21]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;GPIOB->BSRR = (uint32_t) ((0x000000FF & testData[22]) | ((0x000000FF & ~testData[22]) << 16));GPIOA->BSRR = GPIO_PIN_0;
-					GPIOA->BRR = GPIO_PIN_0;
-					thisSystem->BUS_FLAG = 0;
 					//MockData(&thisSystem->Magnetometer);
 					//WriteDataFrame(&thisSystem->Magnetometer, &thisSystem->Bus) ;
 					//GPIOC->BSRR = GPIO_PIN_0;
