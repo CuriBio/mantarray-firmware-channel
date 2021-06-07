9b94e5b54820a587469c1b6035824fe0e2262665
Farshad2021 <farshad@curiio.com>
2021-06-07 10:20:26 -0700

    monday start

--- Debug/Mantarray/Src/mmc5983_driver.d
+++ Debug/Mantarray/Src/mmc5983_driver.d
@@ -32,7 +32,10 @@ Mantarray/Src/mmc5983_driver.o: ../Mantarray/Src/mmc5983_driver.c \
  ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h \
  ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h \
  ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
- ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h
+ ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h \
+ ../Mantarray/inc/GlobalTimer.h ../Mantarray/inc/Magnetometer.h \
+ ../Mantarray/inc/lis3mdl_driver.h ../Mantarray/inc/mmc5983_driver.h \
+ ../Mantarray/inc/GlobalTimer.h
 
 ../Mantarray/inc/mmc5983_driver.h:
 
@@ -103,3 +106,13 @@ Mantarray/Src/mmc5983_driver.o: ../Mantarray/Src/mmc5983_driver.c \
 ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
 
 ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
+
+../Mantarray/inc/GlobalTimer.h:
+
+../Mantarray/inc/Magnetometer.h:
+
+../Mantarray/inc/lis3mdl_driver.h:
+
+../Mantarray/inc/mmc5983_driver.h:
+
+../Mantarray/inc/GlobalTimer.h:
